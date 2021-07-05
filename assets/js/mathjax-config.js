f85499b058b8d697c5614eaad24e16ebdb1fac3f
Charlie Godfrey <cgodfrey@uw.edu>
2021-07-05 11:21:28 -0700

    auto-commit by quick_update.zsh

--- assets/js/mathjax-config.js
+++ assets/js/mathjax-config.js
@@ -2,7 +2,7 @@ MathJax = {
     tex: {
       inlineMath: [              // start/end delimiter pairs for in-line math
         ['\\(', '\\)'],
-        ['$', '$']
+        // ['$', '$']
       ],
       displayMath: [             // start/end delimiter pairs for display math
         ['$$', '$$'],
